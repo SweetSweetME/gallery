8b7a8e7e6d197a84b27b7651e451e8d73cfeb5c2
SweetSweetME <shentiant@outlook.com>
2019-12-07 20:41:16 +0800

    借用构造函数继承

--- src/ES5jc.js
+++ src/ES5jc.js
@@ -1,6 +1,9 @@
 // ES5 6种继承
 // 1. 原型链继承
 // 继承想要继承什么 属性 方法 
+// 2. 借用构造函数  基本思想【在子类的构造函数中调用超类的构造函数 用call】
+// 优点：1. 可以向超类传递参数初始化 2. 解决了原型链继承中包含引用类型值被所有实例共享的问题[刚实验过 基本类型不受影响]
+// 缺点：方法都要在构造函数中定义 函数复用无从谈起 😢 超类原型中定义的属性和方法 对于子类 不可见【即不能用】
 function PersonFemale(hobby = ['唱歌', '敷面膜', '瑜伽']) {
     this.sex = 'female';
     this.hobby = [...hobby];
@@ -27,7 +30,7 @@ var p = new PersonFemale();
 // console.log(p.getSex());
 
 function Student(hobby) {
-    console.log(hobby);
+    // console.log(hobby);
     this.type = 'student';
     // this.hobby = ['学习'];
     this.getType = function () {
@@ -36,20 +39,22 @@ function Student(hobby) {
     this.getSex = function () { // 可以复写
         return 'this.sex';
     }
+    PersonFemale.call(this, hobby); // 借用构造函数
 }
 
 // Student.prototype = PersonFemale.prototype; // 只可以继承 原型上面的属性或者方法
-Student.prototype = new PersonFemale(); // 可以继承实例+原型的方法  可以继承实例+原型的属性 如果本身有该属性直接返回【复写】 没有的话返回父级的 hobby
-Student.prototype.construtor = Student; // ********这一步为什么有没想明白********
+// Student.prototype = new PersonFemale(); // 可以继承实例+原型的方法  可以继承实例+原型的属性 如果本身有该属性直接返回【复写】 没有的话返回父级的 hobby
+
+// Student.prototype.construtor = Student; // ********这一步为什么有没想明白********
 // Student.prototype.getHobby = function () { // 可以复写
 //     return 'this.hobby';
 // }
 
-let bell = new Student();
+let bell = new Student(['哼 我自己也有🐧']);
 // console.log('bell: ', bell.getType());
 // console.log('bell: ', bell.getSex());
-bell.hobby.push('bell新加了一个爱好');
-bell.todo.push('bell有新的TODO');
+// bell.hobby.push('bell新加了一个爱好');
+// bell.todo.push('bell有新的TODO');
 
 // 自己实验完上述的代码后 对下面这句话的理解：
 // 原型链继承的基本思想是利用原型让一个引用类型继承另一个引用类型的属性和方法。
@@ -68,40 +73,51 @@ bell.todo.push('bell有新的TODO');
  * 这里 是将一个实例化对象的属性和方法 赋值给了 Student的原型上 那么 对于Student来说 继承过来的这些属性和方法都放在原型上
  * 说这么多是为了给下方做铺垫
  */
-let lisa = new Student();
-lisa.hobby.push('lisa新加了一个爱好');
-lisa.todo.push('lisa有新的todo');
+let lisa = new Student(['list也有自己的爱好❤️']);
+// lisa.hobby.push('lisa新加了一个爱好');
+lisa.sex = lisa.sex + '基本类型会改变么'
+
+// lisa.todo.push('lisa有新的todo');
 
 let lay = new Student(['lay传递参数初始化hobby']);
-lay.hobby.push('lay新加了一个爱好');
-lay.todo.push('lay有新的todo');
-lay.type = 'Student三好学生';
+// lay.hobby.push('lay新加了一个爱好');
+// lay.todo.push('lay有新的todo');
+// lay.type = 'Student三好学生';
 
 console.log('bell: ', bell.hobby);
 console.log('bell: ', bell.todo);
 console.log('bell: ', bell.type);
+console.log('bell: ', bell.sex);
 console.log('lay: ', lay.hobby);
 console.log('lay: ', lay.todo);
 console.log('lay: ', lay.type);
+console.log('lay: ', lay.sex);
 console.log('lisa: ', lisa.hobby);
 console.log('lisa: ', lisa.todo);
 console.log('lisa: ', lisa.type);
-console.log(Student.prototype.construtor);
+console.log('lisa: ', lisa.sex);
+console.log(bell);
 
 // 继承的属性和方法 其中一个有变动 都会影响所有的改变 不管相对于父级来说是 原型属性还是实例属性
 // 原型属性是共享的 那么 同时改变 是可以 的  但是原来父级的实例属性 最好不共享
 // 那么为什么会共享呢？ 因为是一个实例【引用类型】 赋值给了Student.prototype
 
-// undefined
-// undefined
-// ['lay传递参数初始化hobby']
-// bell: ['唱歌', '敷面膜', '瑜伽', 'bell新加了一个爱好', 'lisa新加了一个爱好', 'lay新加了一个爱好']
-// bell: ['郊游', 'bell有新的TODO', 'lisa有新的todo', 'lay有新的todo']
+// bell: ['哼 我自己也有🐧']
+// bell: undefined
 // bell: student
-// lay: ['唱歌', '敷面膜', '瑜伽', 'bell新加了一个爱好', 'lisa新加了一个爱好', 'lay新加了一个爱好']
-// lay: ['郊游', 'bell有新的TODO', 'lisa有新的todo', 'lay有新的todo']
-// lay: Student三好学生
-// lisa: ['唱歌', '敷面膜', '瑜伽', 'bell新加了一个爱好', 'lisa新加了一个爱好', 'lay新加了一个爱好']
-// lisa: ['郊游', 'bell有新的TODO', 'lisa有新的todo', 'lay有新的todo']
+// bell: female
+// lay: ['lay传递参数初始化hobby']
+// lay: undefined
+// lay: student
+// lay: female
+// lisa: ['list也有自己的爱好❤️']
+// lisa: undefined
 // lisa: student
-// [Function: Student]
+// lisa: female基本类型会改变么
+// Student {
+//     type: 'student',
+//         getType: [Function],
+//             getSex: [Function],
+//                 sex: 'female',
+//                     hobby: ['哼 我自己也有🐧']
+// }
