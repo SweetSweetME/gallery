1d187b09360d94b136731f25df0525791b684005
SweetSweetME <shentiant@outlook.com>
2019-12-20 10:26:27 +0800

    http

--- src/pages/HTTP/index.js
+++ src/pages/HTTP/index.js
@@ -3,6 +3,10 @@ import './style.less';
 
 export default class Http extends React.Component {
 
+    componentDidMount() {
+        this.paint();
+    }
+
     uploadImg = () => {
         const formData = new FormData();
         formData.append('file', this.imgUrl.files[0]);
@@ -21,6 +25,16 @@ export default class Http extends React.Component {
         // request.send(formData);
     }
 
+    paint() {
+        const ctx = this.canvas.getContext('2d');
+        console.log(ctx);
+        ctx.fillStyle = 'red';
+        ctx.moveTo(500, 500);
+        ctx.lineTo(600, 600);
+        ctx.stroke();
+        // ctx.fillRect(10, 10, 150, 100);
+    }
+
     render() {
         return (
             // <form method="post" action="http://localhost:8091/posts/1/img" encType="multipart/form-data">
@@ -32,6 +46,7 @@ export default class Http extends React.Component {
                 <div>
                     <p className="textoverflow">两行溢出两行溢出两行溢出两行溢出两行溢出两行溢出两行溢出两行溢出两行溢出两行溢出</p>
                 </div>
+                <canvas ref={canvas => { this.canvas = canvas; }} width="400" height="400"></canvas>
             </div>
 
             // </form>
